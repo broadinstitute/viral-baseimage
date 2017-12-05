bcce604204d9e421f6a56f8b95f2400f0f95f2f3
Danny Park <dpark@broadinstitute.org>
2017-12-05 11:25:09 -0500

    change the install order

--- install-apt_packages.sh
+++ install-apt_packages.sh
@@ -6,23 +6,26 @@ set -e -o pipefail
 # https://github.com/phusion/baseimage-docker/issues/58
 DEBIAN_FRONTEND=noninteractive
 
-# Minimal ubnutu images need the following:
+# Add some basics
 apt-get update
-apt-get install -y -qq lsb-release
+apt-get install -y -qq --no-install-recommends \
+	lsb-release ca-certificates wget rsync curl \
+	python-crcmod less nano vim git locales make \
+	liblz4-tool pigz bzip2 lbzip2
 
-# phusion/baseimage is based on xenial, but in case something changes,
-# auto-detect here:
+# Auto-detect platform
 DEBIAN_PLATFORM="$(lsb_release -c -s)"
 echo "Debian platform: $DEBIAN_PLATFORM"
 
+# Add source for gcloud sdk
 echo "deb http://packages.cloud.google.com/apt cloud-sdk-$DEBIAN_PLATFORM main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
+# Install gcloud and aws
 apt-get install -y -qq --no-install-recommends \
-	ca-certificates wget rsync curl \
-	python-crcmod less nano vim git locales make \
-	google-cloud-sdk awscli \
-	liblz4-tool pigz bzip2 lbzip2
+	google-cloud-sdk awscli
+
+# Upgrade and clean
 apt-get upgrade -y
 apt-get clean
 
