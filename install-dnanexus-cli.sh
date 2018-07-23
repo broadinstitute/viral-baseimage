8403926c934d9c7189ccb8c1b0119d80a7706f6b
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2018-07-23 12:36:00 -0400

    update dx-toolkit and ua

--- install-dnanexus-cli.sh
+++ install-dnanexus-cli.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # get the dx-toolkit
-curl -s https://wiki.dnanexus.com/images/files/dx-toolkit-v0.237.0-ubuntu-14.04-amd64.tar.gz | tar -xzp
+curl -s https://wiki.dnanexus.com/images/files/dx-toolkit-v0.255.0-ubuntu-14.04-amd64.tar.gz | tar -xzp
 
 #get the ua
-curl -s https://wiki.dnanexus.com/images/files/dnanexus-upload-agent-1.5.29-linux.tar.gz | tar -xzp
+curl -s https://wiki.dnanexus.com/images/files/dnanexus-upload-agent-1.5.30-linux.tar.gz | tar -xzp
 mv dnanexus-upload-agent-1.5.29-linux/ua dx-toolkit/bin
 rmdir dnanexus-upload-agent-1.5.29-linux
 
