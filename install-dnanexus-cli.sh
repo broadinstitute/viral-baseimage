1d74908cf1b1ce2c6d2babe039448b8065fdaef8
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-07-03 15:24:51 -0400

    update dx download URLs

--- install-dnanexus-cli.sh
+++ install-dnanexus-cli.sh
@@ -6,10 +6,10 @@ DX_TOOLKIT_VERSION=0.285.0
 DX_UPLOAD_AGENT_VERSION=1.5.31
 
 # get the dx-toolkit
-curl -s https://wiki.dnanexus.com/images/files/dx-toolkit-v${DX_TOOLKIT_VERSION}-ubuntu-14.04-amd64.tar.gz | tar -xzp
+curl -s https://dnanexus-sdk.s3.amazonaws.com/dx-toolkit-v${DX_TOOLKIT_VERSION}-ubuntu-14.04-amd64.tar.gz | tar -xzp
 
 #get the ua
-curl -s https://wiki.dnanexus.com/images/files/dnanexus-upload-agent-${DX_UPLOAD_AGENT_VERSION}-linux.tar.gz | tar -xzp
+curl -s https://dnanexus-sdk.s3.amazonaws.com/dnanexus-upload-agent-${DX_UPLOAD_AGENT_VERSION}-linux.tar.gz | tar -xzp
 mv dnanexus-upload-agent-${DX_UPLOAD_AGENT_VERSION}-linux/ua dx-toolkit/bin
 rmdir dnanexus-upload-agent-${DX_UPLOAD_AGENT_VERSION}-linux
 
