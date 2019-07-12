586c93d0204d421f3e3a72c80c79d13000caf92f
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2019-07-12 16:39:54 -0400

    pin conda version via install script
    pin conda version via install script rather than downgrade of base; increase precedence of conda-forge channel

--- install-miniconda.sh
+++ install-miniconda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -o pipefail
 
-MINICONDA_URL=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+MINICONDA_URL=https://repo.continuum.io/miniconda/Miniconda3-4.6.14-Linux-x86_64.sh
 
 # download and run miniconda installer script
 curl -sSL $MINICONDA_URL > "/tmp/Miniconda3-latest-x86_64.sh"
@@ -14,9 +14,8 @@ hash -r
 conda config --set always_yes yes --set changeps1 no
 conda config --add channels r
 conda config --add channels defaults
-conda config --add channels conda-forge
 conda config --add channels bioconda
+conda config --add channels conda-forge
 conda config --add channels broad-viral
-conda install -q -y -c defaults conda==4.6.14
 conda config --set auto_update_conda false
 conda clean -y --all
