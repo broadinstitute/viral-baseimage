e74fcedb6c47729a1f8cf6650771040eb9198f6d
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2023-02-17 09:05:47 -0500

    script comment

--- install-dnanexus-cli.sh
+++ install-dnanexus-cli.sh
@@ -8,7 +8,7 @@ DX_UPLOAD_AGENT_VERSION=1.5.33
 # get the dx-toolkit
 curl -s https://dnanexus-sdk.s3.amazonaws.com/dx-toolkit-v${DX_TOOLKIT_VERSION}-ubuntu-16.04-amd64.tar.gz | tar -xzp
 
-#get the ua
+#get the dnanexus upload agent (ua agent)
 curl -s https://dnanexus-sdk.s3.amazonaws.com/dnanexus-upload-agent-${DX_UPLOAD_AGENT_VERSION}-linux.tar.gz | tar -xzp
 mv dnanexus-upload-agent-${DX_UPLOAD_AGENT_VERSION}-linux/ua dx-toolkit/bin
 rmdir dnanexus-upload-agent-${DX_UPLOAD_AGENT_VERSION}-linux
